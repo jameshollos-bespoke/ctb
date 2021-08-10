b2e1dd8aa28e7ab705f557d4a5091f889c06addf
jameshollos-bespoke <james@bespokedigital.agency>
2021-08-10 14:29:16 +0100

    Removed unnecessary tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,6 @@
 const gulp = require("gulp");
 const imgMin = require("gulp-imagemin");
 const uglify = require('gulp-uglify-es').default;
-const watch = require('gulp-watch');
-
-//Minify all img
-gulp.task("imgMin", async () => {
-    gulp.src("src/img/*")
-    .pipe(imgMin())
-    .pipe(gulp.dest("dist/img"))
-
-    console.log("Minifying images...");
-})
-
-//Uglify JS
-gulp.task("uglify", async () => {
-    gulp.src("src/js/*.js")
-    .pipe(uglify())
-    .pipe(gulp.dest("dist/js/"))
-    console.log("Uglifying JS...");
-})
 
 function defaultTask(cb){
     gulp.src("src/js/*.js")
